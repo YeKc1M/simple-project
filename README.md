ef77b165e21d399af8c44dba2cc754e4837940eb
YeKc1M <15008181891@163.com>
2019-06-23 22:44:38 +0800

    add sp2.cpp, PigLatin game implemented in c++

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 this branch is used to save codes of some simple project
 
 sp1.cpp reverse a string
+
+sp2.cpp Pig Latin game
---
+++ sp2.cpp
@@ -0,0 +1,41 @@
+﻿// sp2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isVowel(const char & c);
+void PigLatin(const string & str, string & pl)
+{
+	string s = str;
+	int i = 0;
+	for (char c : s)
+	{
+		if (!isVowel(c))
+			break;
+		i++;
+	}
+	char c = s[i];
+	for (int j = i; j < s.length() - 1; j++)
+		s[j] = s[j + 1];
+	s[s.length() - 1] = c;
+	s += "ay";
+	pl = s;
+}
+
+int main()
+{
+	string s;
+	cout << "input string: ";
+	cin >> s;
+	string pl;
+	PigLatin(s, pl);
+	cout << pl << endl;
+}
+
+bool isVowel(const char & c)
+{
+	return(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y');
+}
