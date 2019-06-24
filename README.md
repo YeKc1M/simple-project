ce0586c25105aaf31f8f3051112bf8b0e3a0cfd5
YeKc1M <15008181891@163.com>
2019-06-24 12:11:17 +0800

    sp4.cpp palindrome string

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ sp1.cpp reverse a string
 sp2.cpp Pig Latin game
 
 sp3.cpp count the number of each vowel in a string
+
+sp4.cpp palindrome string
---
+++ sp4.cpp
@@ -0,0 +1,29 @@
+﻿// sp4.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool palindrome(const string & s);
+
+int main()
+{
+	string s;
+	cout << "input a string: ";
+	cin >> s;
+	cout << palindrome(s) << endl;
+}
+
+bool palindrome(const string & s)
+{
+	bool flag = true;
+	for (int i = 0; i < s.length() / 2; i++)
+		if (s[i] != s[s.length() - 1 - i])
+		{
+			flag = false;
+			break;
+		}
+	return flag;
+}
